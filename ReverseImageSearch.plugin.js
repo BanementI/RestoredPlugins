4cbbc84be7ba13948854a5ebc84925c5ea621c25
null <47301693+BanementI@users.noreply.github.com>
2020-08-15 23:13:13 +0100

    Update ReverseImageSearch.plugin.js
    replaced new bdfdb.min.js source with my own older version

--- ReverseImageSearch.plugin.js
+++ ReverseImageSearch.plugin.js
@@ -92,7 +92,7 @@ var ReverseImageSearch = (_ => {
 				libraryScript = document.createElement("script");
 				libraryScript.setAttribute("id", "BDFDBLibraryScript");
 				libraryScript.setAttribute("type", "text/javascript");
-				libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.min.js");
+				libraryScript.setAttribute("src", "https://raw.githubusercontent.com/BanementI/RestoredPlugins/master/BDFDB.min.js");
 				libraryScript.setAttribute("date", performance.now());
 				libraryScript.addEventListener("load", _ => {this.initialize();});
 				document.head.appendChild(libraryScript);
